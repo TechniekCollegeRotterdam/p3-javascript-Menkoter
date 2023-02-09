ac9623a57f779ad47838173cab79347d7512796c
Menkoter <9018140@student.zadkine.nl>
2023-02-09 13:28:56 +0100

    week 2

---
+++ opdracht 2.4/app.js
@@ -0,0 +1,29 @@
+
+function calculate(num , num2) {
+let answer = num + num2
+console.log(answer);
+
+
+}
+
+calculate(50,120);
+
+//opdracht2
+function multiplyByFive(num){
+    let answer = num * 5;
+    console.log(answer)
+}
+
+multiplyByFive(85);
+
+
+//opdracht 3 
+
+function calculateminutesnum (num1, num2) {
+    let answer = num / 60 ;
+    console.log (answer)
+}
+
+calculateMinutes(3600)
+
+
---
+++ week 2 uitleg/app.js
@@ -0,0 +1,31 @@
+
+let title = document.getElementById("title");
+
+.style.color = "blue";
+
+let image = document.getElementById("image");
+
+let button = document.getElementById("button");
+
+function removeImage() {
+    
+    
+}
+
+.style.display = "none";
+title.style.color = "blue";
+image.style.display = "none";
+
+function showImage(){
+    title.style.color = "black";
+    image.style.display = "block";
+}
+
+
+function calc(num, num2) {
+let answer = num - num2; 
+console.log(answer);
+
+}
+
+calc(7,8); 
