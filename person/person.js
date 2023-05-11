e6707fbcdaf26409c77e1b45d9ff89c918473eca
Menkoter <9018140@student.zadkine.nl>
2023-05-11 11:53:22 +0200

    persons

---
+++ person/person.js
@@ -0,0 +1,24 @@
+let output = document.querySelector(".output");
+
+let person = {
+name : "Menno",
+leeftijd : "17",
+Geboortedatum : "18-08-2005",
+Geslacht : "Man",
+In leven : "Ja",
+Hobby’s : "Varen, Gamen, Lopen, Tekenen",
+}
+
+output.innerHTML = ` 
+<p>His hobbies are ${hobby.Varen}</p>
+<p>His hobbies are ${hobby.Gamen}</p>
+<p>His hobbies are ${hobby.Lopen}</p>
+<p>His hobbies are ${hobby.Tekenen}</p>
+
+
+
+
+
+
+
+`
