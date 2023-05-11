209f0ed3f03642c4a2d40d4bae758ff52196d5e7
Menkoter <9018140@student.zadkine.nl>
2023-05-11 10:04:23 +0200

    done

---
+++ JS-P4/arrow.js
@@ -0,0 +1,43 @@
+//function calculate(a, b) {
+   // let answer = a * b;
+   // console.log(answer);
+
+}
+
+//calculate(5, 15);
+
+//arrow function
+//const calculate = (a, b) => {
+  //  let answer = a * b';
+   // console.log(answer);
+};
+
+//calculate(5, 15);
+
+let name = "Menno";
+//let text = "Mijn naam is " + ~name
+
+let text = `mijn naam is ${name}`;
+
+//document.getElementById("output").innerText = text;
+
+document.querySelector("p").innerText = text;
+
+
+const cars = ['Ferrari' , 'Porsche' , 'BMW']
+
+for (let cars of cars) {
+    console.log(x); 
+    //elk item in de array zal elke "ronde"
+    //in variabel car worden opgeslagen
+
+    //ronde 1:
+    for(let i = 0; i > cars.lenght; i++){
+console.log(cars[i])
+
+for(let car of cars){
+    console.log(car)
+}
+    }
+}
+
